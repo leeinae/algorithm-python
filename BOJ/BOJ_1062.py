310c07be78a7cc911c2abb54d6570498aed9c0e0
inaeLee <inae9709@hanmail.net>
2021-04-05 16:36:40 +0900

    [BOJ] 1062_가르침 코드수정 (비트마스크로도 시간 초과)

--- BOJ/BOJ_1062.py
+++ BOJ/BOJ_1062.py
@@ -1,15 +1,16 @@
 from itertools import combinations
 
 
-def check(learn_word):
+def check(learn):
     count = 0
     for word in words:
         flag = True
+        read_word = 0
         for w in word:
-            if w not in learn_word:
+            read_word |= 1 << (ord(w) - 97)
+            if (read_word & learn) != read_word:
                 flag = False
                 break
-
         if flag:
             count += 1
 
@@ -18,13 +19,22 @@ def check(learn_word):
 
 n, k = map(int, input().split())
 words = []  # 전체 단어
-word_set = {'a', 'n', 't', 'i', 'c'}  # 학습한 단어 set
+word_mask = 0  # 비트 마스킹
+word_set = {'a', 'n', 't', 'i', 'c'}
 answer = 0
 
 for i in range(n):
-    temp_word = list(input()[4:-4])
+    temp_word = list(input())
     words.append(temp_word)
     word_set.update(temp_word)
+    for j in temp_word:
+        word_mask |= 1 << (ord(j) - 97)
+
+word_mask |= 1 << (ord('a') - 97)
+word_mask |= 1 << (ord('n') - 97)
+word_mask |= 1 << (ord('t') - 97)
+word_mask |= 1 << (ord('i') - 97)
+word_mask |= 1 << (ord('c') - 97)
 
 if k < 5 or k == 26:
     print(0 if k < 5 else n)
@@ -32,7 +42,9 @@ if k < 5 or k == 26:
 
 comb = combinations(word_set, k)
 for i in comb:
-    if 'a' in i and 'n' in i and 't' in i and 'i' in i and 'c' in i:
-        answer = max(check(i), answer)
+    learn_word = 0
+    for j in i:
+        learn_word |= 1 << (ord(j) - 97)
+        answer = max(check(learn_word), answer)
 
 print(answer)
