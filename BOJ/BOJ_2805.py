b754feb9b1ba0a9f8e0f2823b92e7b8475efcac4
inaeLee <inae9709@hanmail.net>
2021-04-07 23:41:16 +0900

    [BOJ] 2805_나무 자르기_21/04/07

---
+++ BOJ/BOJ_2805.py
@@ -0,0 +1,25 @@
+n, m = map(int, input().split())
+trees = list(map(int, input().split()))
+
+start = 0
+end = max(trees)
+
+
+def cut_tree(h):
+    result = 0
+    for tree in trees:
+        if tree > h:
+            result += (tree - h)
+    return result
+
+
+while start <= end:
+    middle = (start + end) // 2  # 높이
+    result = cut_tree(middle)
+
+    if result >= m:
+        start = middle + 1
+    else:
+        end = middle - 1
+
+print(end)
