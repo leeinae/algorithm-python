8527a4c2930366e6151f09365ed0f0365c714824
inaeLee <inae9709@hanmail.net>
2021-05-13 17:51:30 +0900

    fix: [1845] 불필요한 코드 줄이기

--- Programmers/1845.py
+++ Programmers/1845.py
@@ -1,15 +1,5 @@
 def solution(nums):
-    answer = 0
-    num_count = len(set(nums))
-
-    if num_count >= len(nums) // 2:
-        answer = len(nums) // 2
-    else:
-        answer = num_count
-
-    print(answer)
-    return answer
-
+    return len(nums) // 2 if len(set(nums)) >= len(nums) // 2 else len(set(nums))
 
 solution([3, 1, 2, 3])
 solution([3, 3, 3, 2, 2, 2])
