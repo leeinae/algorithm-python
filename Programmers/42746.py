9ffef4fe7769f94ab8ebf00286fbe6b4da7d34f9
inaeLee <inae9709@hanmail.net>
2021-06-11 02:08:24 +0900

    [PRO] 42746_가장 큰 수_21/06/11

---
+++ Programmers/42746.py
@@ -0,0 +1,10 @@
+# 프로그래머스 가장 큰 수........ 풀이 참고함
+def solution(numbers):
+    numbers = sorted(list(map(str, numbers)), key=lambda x: x * 2, reverse=True)
+    return str(int("".join(numbers)))
+
+print(solution([3, 30, 34, 5, 9]))
+print(solution([6, 10, 2, 20]))
+print(solution([0, 0, 0, 0]))
+print(solution([1, 10, 20]))
+print(solution([2, 20, 22]))
