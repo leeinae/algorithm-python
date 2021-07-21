d358810938d616203c28a8039bf32995dab5f61b
inaeLee <inae9709@hanmail.net>
2021-07-21 11:28:27 +0900

    [PRO] 12951_JadenCase 문자열 만들기_21/07/21

---
+++ Programmers/12951.py
@@ -0,0 +1,7 @@
+def solution(s):
+    answer = " ".join(list(map(lambda x: x.capitalize(), s.split(" "))))
+    return answer
+
+
+solution("3people unFollowed me")
+solution(" a  sdfsdf Ssdlfkmd SDSDFkkdfsd")
