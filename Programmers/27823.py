39a1c99768f8a6163094a01715d385a826beb539
inaeLee <inae9709@hanmail.net>
2021-07-29 16:00:01 +0900

    [PRO] 완주하지 못한 선수_노가다버전_21/07/29

--- Programmers/27823.py
+++ Programmers/27823.py
@@ -4,6 +4,19 @@ from collections import Counter
 def solution(participant, completion):
     answer = Counter(participant) - Counter(completion)
 
+    p_dict, c_dict = dict(), dict()
+    for p in participant:
+        if p in p_dict:
+            p_dict[p] += 1
+        else:
+            p_dict[p] = 1
+
+    for c in completion:
+        if c in c_dict:
+            c_dict[c] += 1
+        else:
+            c_dict[c] = 1
+
     return list(answer.keys())[0]
 
 
