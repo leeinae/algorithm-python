ec0769ccda66c309e268257195964a5d62ba262a
inaeLee <inae9709@hanmail.net>
2021-08-02 21:30:39 +0900

    fix: 80612_더 맵게 try, except 사용해 코드 개선

--- Programmers/80612.py
+++ Programmers/80612.py
@@ -7,16 +7,13 @@ def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
 
-    while scoville:
-        first = heapq.heappop(scoville)
-        if first >= K: break
-
-        if len(scoville) > 0:
-            answer += 1
-            second = heapq.heappop(scoville)
+    while scoville[0] < K:
+        try:
+            first, second = heapq.heappop(scoville), heapq.heappop(scoville)
             heapq.heappush(scoville, first + (second * 2))
-    else:
-        return -1
+            answer += 1
+        except:
+            return -1
 
     return answer
 
