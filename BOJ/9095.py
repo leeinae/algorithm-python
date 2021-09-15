9af2c498d7a7e00d88eb45afbcd936e6aef0147a
inaeLee <inae9709@hanmail.net>
2021-09-15 12:20:15 +0900

    [BOJ] 9095_DP 풀이

--- BOJ/9095.py
+++ BOJ/9095.py
@@ -16,5 +16,11 @@ test_case = int(input())
 for t in range(test_case):
     n = int(input())
     answer = 0
-    add_num(0, 0)
-    print(answer)
+
+    dp = [0] * 11
+    dp[1], dp[2], dp[3] = 1, 2, 4
+
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    print(dp[n])
