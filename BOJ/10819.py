75a2c2c6973815e377cd5781ea5b62c9d0186aed
inaeLee <inae9709@hanmail.net>
2021-09-17 12:28:06 +0900

    [BOJ] 10819_차이를 최대로_21/09/17

---
+++ BOJ/10819.py
@@ -0,0 +1,20 @@
+def search(total, curr, prev, count, visited):
+    global answer, n, nums
+    total = total + abs(curr - prev) if count != 1 else 0
+    if count == n:
+        answer = max(answer, total)
+        return
+
+    for i in range(len(nums)):
+        if not visited[i]:
+            visited[i] = True
+            search(total, nums[i], curr, count + 1, visited)
+            visited[i] = False
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+visited = [False] * len(nums)
+answer = 0
+search(0, 0, 0, 0, visited)
+print(answer)
